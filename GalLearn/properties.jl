103fb50d415b4846f456934b77eacf7f756cdc61
patricks1 <patrickstaudt1@gmail.com>
2024-04-03 18:19:13 -0700

    fixed errors in properties.jl so it identifies host IDs now

--- GalLearn/properties.jl
+++ GalLearn/properties.jl
@@ -4,13 +4,18 @@ import PyCall
 direc = "/DFS-L/DATA/cosmo/jgmoren1/FIREBox/FB15N1024/"
 
 function get_hosts()
-    fname = direc * "global_sample_data_snapshot_1200.hdf5"
+    fname = direc * "global_sample_data/global_sample_data_snapshot_1200.hdf5"
     grp_ids, gal_ids = HDF5.h5open(fname) do file
-        grp_ids = read(file, "groundID")
+        grp_ids = read(file, "groupID")
         gal_ids = read(file, "galaxyID")
         return grp_ids, gal_ids
+    end
 
-    is_host = grp_ids == -1
+    is_host = grp_ids .== -1
     host_ids = gal_ids[is_host]
+    println(String("N hosts: $(length(host_ids))"))
 
     return host_ids
+end
+
+host_ids = get_hosts()
