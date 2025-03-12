f08fc06bc17ab1a5ce9a7da27e86dba901207335
Patrick Staudt <patrickstaudt1@gmail.com>
2025-03-12 16:44:38 -0700

    Seeing what happens with a fast lr

--- GalLearn/cnn.py
+++ GalLearn/cnn.py
@@ -839,7 +839,7 @@ def main(Nfiles=None, wandb_mode='n', run_name=None):
         net_type = 'ResNet'
 
         # Things wandb will track
-        lr = 5.e-4 # learning rate
+        lr = 5.e-2 # learning rate
         momentum = 0.5
         activation_module = nn.ReLU
         dataset = 'gallearn_data_256x256_3proj_wsat_2d_tgt.h5'
