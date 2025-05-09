12baaa850d594d85098947b3807ca547acc05ccb
Patrick Staudt <patrickstaudt1@gmail.com>
2025-05-08 20:25:58 -0700

    Switching to ResNet-18

--- GalLearn/cnn.py
+++ GalLearn/cnn.py
@@ -959,8 +959,8 @@ def main(Nfiles=None, wandb_mode='n', run_name=None):
         momentum = 0.5
         dataset = 'gallearn_data_256x256_3proj_wsat_2d_tgt.h5'
         #dataset = 'ellipses.h5'
-        n_blocks_list = [3, 4, 6, 3]
-        resblock = BottleNeck
+        n_blocks_list = [2, 2, 2, 2]
+        resblock = BasicResBlock 
 
         # Other things
         N_out_channels = 1
